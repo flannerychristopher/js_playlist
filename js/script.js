1bcc8d1cc8057a5a92e2e21e91817459a61b419b
chris flan <flanthedev@gmail.com>
2017-08-06 17:19:41 +0800

    finish Playlist prototype functions

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
+const listElement = document.getElementById('playList');
+const playList = new Playlist();
+const song1 = new Song("Hey Ya", "Outkast", "3:01");
+const song2 = new Song("Me and My Girlfriend", "2pac", "2:55");
+const song3 = new Song("Hotel California", "Eagles", "4:35");
+
 function Song(title, artist, duration) {
   this.title = title;
   this.artist = artist;
   this.duration = duration;
   this.isPlaying = false;
 }
-
 Song.prototype.play = function() {
   this.isPlaying = true;
 };
@@ -25,3 +30,34 @@ function Playlist() {
   this.songs = [];
   this.nowPlayingIndex = 0;
 }
+
+Playlist.prototype.add = function(song) {
+  this.songs.push(song);
+};
+Playlist.prototype.play = function() {
+  let currentSong = this.songs[this.nowPlayingIndex];
+  currentSong.play();
+};
+Playlist.prototype.stop = function() {
+  let currentSong = this.songs[this.nowPlayingIndex];
+  currentSong.stop();
+};
+Playlist.prototype.next = function() {
+  this.stop();
+  this.nowPlayingIndex++;
+  if(this.nowPlayingIndex === this.songs.length) {
+    this.nowPlayingIndex = 0;
+  }
+  this.play();
+};
+Playlist.prototype.render = function(list) {
+  list.innerHTML = '';
+  for(i = 0; i < this.songs.length; i++) {
+    list.innerHTML += this.songs[i].toHTML;
+  }
+};
+
+playList.add(song1);
+playList.add(song2);
+playList.add(song3);
+playList.render(listElement);
